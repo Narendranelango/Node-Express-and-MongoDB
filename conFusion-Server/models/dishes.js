b5d5c1d80eb5bcf20a7e48bf7431e51898a5d209
Narendran Elango <41041022+Narendranelango@users.noreply.github.com>
2021-08-19 00:25:12 +0530

    Week-2 Express REST API with MongoDB and Mongoose Part 1

--- conFusion-Server/models/dishes.js
+++ conFusion-Server/models/dishes.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
 var commentSchema = new Schema({
     rating:  {
         type: Number,
@@ -20,7 +23,7 @@ var commentSchema = new Schema({
     timestamps: true
 });
 
-var dishSchema = new Schema({
+const dishSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -30,8 +33,29 @@ var dishSchema = new Schema({
         type: String,
         required: true
     },
-    comments:[commentSchema]//Contains the comment schema which is present from 18 to 35.
-}, {                        //End of Schema and start of timestamps
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+    comments:[commentSchema]
+}, {
     timestamps: true
 });
 
