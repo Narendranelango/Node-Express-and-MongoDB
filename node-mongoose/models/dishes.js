6915b512a4d59819920e28c2975439b2082bd18d
Narendran Elango <41041022+Narendranelango@users.noreply.github.com>
2021-08-18 21:11:02 +0530

    Week-2 Mongoose Part 3

--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const dishSchema = new Schema({
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var dishSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -11,7 +30,8 @@ const dishSchema = new Schema({
         type: String,
         required: true
     },
-},{                         //End of Schema and start of timestamps
+    comments:[commentSchema]//Contains the comment schema which is present from 18 to 35.
+}, {                        //End of Schema and start of timestamps
     timestamps: true
 });
 
