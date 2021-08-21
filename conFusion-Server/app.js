494b249ca78d250ab62b2d9dae5dbc5fc4996786
Narendran Elango <41041022+Narendranelango@users.noreply.github.com>
2021-08-22 02:19:44 +0530

    Week-3 Passport

--- conFusion-Server/app.js
+++ conFusion-Server/app.js
@@ -3,16 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+const Promotions = require('./models/promotions');
+const Leaders = require('./models/leaders');
+
+const url = 'mongodb://localhost:27017/MongoDB';
+
+const connect = mongoose.connect(url);
+connect.then((db) => {
+  console.log('Connected currently to the server');
+},(err)=>{console.log(err);});
 
 var app = express();
 
@@ -23,62 +36,43 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('12345-67890-09876-54321'));//Unique value
+// app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({       //Using session instead of cookieParser
+app.use(session({
   name: 'session-id',
-  secret: '12345-67890-09876-54321',//same as used in cookieParser
+  secret: '12345-67890-09876-54321',
   saveUninitialized: false,
   resave: false,
   store: new FileStore()
 }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);//From line 8.
-
-function auth (req, res, next) {
-    console.log(req.session);
-
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
-  }
-}
-
-app.use(auth);//To use the auth function before accessing the URL(i.e. line 53-60 ).
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+function auth(req,res,next){
+
+  if(!req.user){
+    var err = new Error('You are not authenticated!');
+    err.status = 401; 
+    return next(err);
+  }
+
+  else{
+    next();
+  }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 
-const mongoose = require('mongoose');
-
-const Dishes = require('./models/dishes');
-const Promotion = require('./models/promotions');
-const Leaders = require('./models/leaders');
-
-const url = 'mongodb://localhost:27017/MongoDB';
-const connect = mongoose.connect(url);
-
-connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
